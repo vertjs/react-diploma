008196a2789c07a6aae660c17fc347fb1b6b398f
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-09-15 21:07:57 +0300

    App.css

--- frontend/src/components/HeadPage.js
+++ frontend/src/components/HeadPage.js
@@ -10,35 +10,37 @@ function App() {
         <div className="row">
             <div className="col">
             <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <NavLink to='/' exact><img src={headerLogo} alt="Bosa Noga" /></NavLink>
-                <div className="collapase navbar-collapse" id="navbarMain">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <NavLink to='/' exact className="nav-link">Главная</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to='/catalog' exact className="nav-link">Каталог</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to='/about' exact className="nav-link">О магазине</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to='/contacts' exact className="nav-link">Контакты</NavLink>
-                        </li>
-                    </ul>
-                    <div>
-                        <div className="header-controls-pics">
-                            <div data-id="search-expander" className="header-controls-pic header-controls-search"></div>
-                            {/*<!-- Do programmatic navigation on click to /cart.html -->*/}
-                            <div className="header-controls-pic header-controls-cart">
-                                <div className="header-controls-cart-full">1</div>
-                                <div className="header-controls-cart-menu"></div>
-                            </div>
+            <NavLink to='/' exact className="navbar-brand">
+                <img src={headerLogo} alt="Bosa Noga" />
+            </NavLink>
+            <div className="collapase navbar-collapse" id="navbarMain">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <NavLink to='/' exact className="nav-link">Главная</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/catalog' exact className="nav-link">Каталог</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/about' exact className="nav-link">О магазине</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/contacts' exact className="nav-link">Контакты</NavLink>
+                    </li>
+                </ul>
+                <div>
+                    <div className="header-controls-pics">
+                        <div data-id="search-expander" className="header-controls-pic header-controls-search"></div>
+                        {/*<!-- Do programmatic navigation on click to /cart.html -->*/}
+                        <div className="header-controls-pic header-controls-cart">
+                            <div className="header-controls-cart-full">1</div>
+                            <div className="header-controls-cart-menu"></div>
                         </div>
-                        <form data-id="search-form" className="header-controls-search-form form-inline invisible">
-                            <input className="form-control" placeholder="Поиск"/>
-                        </form>
                     </div>
+                    <form data-id="search-form" className="header-controls-search-form form-inline invisible">
+                        <input className="form-control" placeholder="Поиск"/>
+                    </form>
+                </div>
                 </div>
             </nav>
             </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
 import App from '../src/components/App';
 import * as serviceWorker from './serviceWorker';
 
