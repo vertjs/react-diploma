b7e6f75ce8ac6b1669db0e5a3999654a912f9a7f
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-09-28 22:18:04 +0300

    ProductPage. Особые случаи: если ни одного размера не доступно - доработать + ссылку в корзину

--- frontend/src/components/ProductPage.js
+++ frontend/src/components/ProductPage.js
@@ -1,15 +1,15 @@
 import React, {useState, useEffect, Fragment} from 'react'
-//import { useSelector, useDispatch } from 'react-redux'
-//import { fetchDataProduct } from '../actions/actionCreators'
 import useJsonFetch from '../hooks/useJsonFetch'
-
 
 export default function ProductPage({match}) {
     let matchId = (match.params.id)
     let num = matchId.match(/\d+/)
     const url = process.env.REACT_APP_DATA_CATEGORIES_URL + '/' + num[0]
     const [data] = useJsonFetch(url, {})
-    
+    const [selected, setSelected] = useState(false) // выбран размер
+    const [amount, setAmount] = useState(0) // количество товара
+    const [mark, setMark] = useState(false) // флаг для стиля кнопки корзины
+
     const [form, setForm] = useState({
         image: '',
         title: '',
@@ -36,64 +36,93 @@ export default function ProductPage({match}) {
             })
         }
     }, [data])
-   
+
+    const handleSelected = () => { // выделить выбранный размер
+        setSelected(!selected)
+     
+    }
+
+    const handleDecrement = () => { // уменьшить количество товаров в корзине
+        if(amount === 0 ) {
+            setAmount(0)
+        } else if(amount === 1) {
+            setAmount(amount => amount - 1)
+            setMark(false)
+        }
+        else{
+            setAmount(amount => amount - 1)
+        }
+    }
+
+    const handleIncrement = () => { // увеличить количество товаров в корзине
+        if(amount === 10) {
+            setAmount(10)
+        } else {
+            setAmount(amount => amount + 1)
+            setMark(true)
+        }
+    }  
 
     return (
         <Fragment>
-        {form.title !== undefined &&
-        <section className="catalog-item">
-            <h2 className="text-center">{form.title}</h2>
-            <div className="row">
-                <div className="col-5">
-                    <img src={form.image}
-                        className="img-fluid" alt={form.title}/>
-                </div>
-                <div className="col-7">
-                    <table className="table table-bordered">
-                        <tbody>
-                            <tr>
-                                <td>Артикул</td>
-                                <td>{form.sku}</td>
-                            </tr>
-                            <tr>
-                                <td>Производитель</td>
-                                <td>{form.manufacturer}</td>
-                            </tr>
-                            <tr>
-                                <td>Цвет</td>
-                                <td>{form.color}</td>
-                            </tr>
-                            <tr>
-                                <td>Материалы</td>
-                                <td>{form.material}</td>
-                            </tr>
-                            <tr>
-                                <td>Сезон</td>
-                                <td>{form.season}</td>
-                            </tr>
-                            <tr>
-                                <td>Повод</td>
-                                <td>{form.reason}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div className="text-center">
-                        <p>Размеры в наличии: 
-                            { form.sizes !== undefined && form.sizes.map((o, i) => o.avalible && <span className="catalog-item-size selected" key={i}>{o.size}</span>) }
-                        </p> 
-                        <p>Количество: <span className="btn-group btn-group-sm pl-2">
-                                <button className="btn btn-secondary">-</button>
-                                <span className="btn btn-outline-primary">1</span>
-                                <button className="btn btn-secondary">+</button>
-                            </span>
-                        </p>
-                    </div>
-                    <button className="btn btn-danger btn-block btn-lg">В корзину</button>
-                </div> 
-            </div> 
-        </section>
-        }
+            {form.title !== undefined &&
+                <section className="catalog-item container catalog">
+                    <h2 className="text-center">{form.title}</h2>
+                    <div className="row">
+                        <div className="col-5">
+                            <img src={form.image}
+                                className="img-fluid" alt={form.title}/>
+                        </div>
+                        <div className="col-7">
+                            <table className="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <td>Артикул</td>
+                                        <td>{form.sku}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Производитель</td>
+                                        <td>{form.manufacturer}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Цвет</td>
+                                        <td>{form.color}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Материалы</td>
+                                        <td>{form.material}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Сезон</td>
+                                        <td>{form.season}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Повод</td>
+                                        <td>{form.reason}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div className="text-center">
+                                <p>Размеры в наличии: 
+                                    { form.sizes !== undefined && 
+                                    form.sizes.map((o, i) => o.avalible && 
+                                        <span className={`catalog-item-size ${selected ? 'selected' : ''} `} key={i} onClick={handleSelected}>
+                                            {o.size}
+                                        </span> )
+                                    }
+                                </p> 
+                                <p>Количество: <span className="btn-group btn-group-sm pl-2">
+                                        <button className="btn btn-secondary" onClick={handleDecrement}>-</button>
+                                        <span className="btn btn-outline-primary">{amount}</span>
+                                        <button className="btn btn-secondary" onClick={handleIncrement}>+</button>
+                                    </span>
+                                </p>
+                            </div>
+                            <button className='btn btn-danger btn-block btn-lg' disabled={mark && selected ? false : true} >В корзину</button>
+                        </div> 
+                    </div> 
+                </section>
+            }
         </Fragment>
-    )
-            
+    )        
 }
